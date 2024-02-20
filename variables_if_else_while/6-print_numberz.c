e0b3490303142f055738be9e8097b66957389049
seliisyl <8859@holbertonstudents.com>
2024-02-20 20:22:29 +0100

    6-print_numberz.c

--- variables_if_else_while/6-print_numberz.c
+++ variables_if_else_while/6-print_numberz.c
@@ -8,16 +8,11 @@
  */
 int main(void)
 {
-	putchar('0' + 0);
-	putchar('0' + 1);
-	putchar('0' + 2);
-	putchar('0' + 3);
-	putchar('0' + 4);
-	putchar('0' + 5);
-	putchar('0' + 6);
-	putchar('0' + 7);
-	putchar('0' + 8);
-	putchar('0' + 9);
+	int num;
+
+	for (num = 0; num < 10; num++)
+		putchar((num % 10) + '0');
+
 	putchar('\n');
 	return (0);
 }
