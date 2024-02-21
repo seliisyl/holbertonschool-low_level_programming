99c1d5e9979eac01363597829c0854314a6dcf8e
seliisyl <8859@holbertonstudents.com>
2024-02-21 13:19:59 +0100

    0-putchar.c

---
+++ functions_nested_loop/0-putchar.c
@@ -0,0 +1,20 @@
+#include <unistd.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point of my code
+ *
+ * return: Always 0
+ */
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('a');
+	_putchar('o');
+	_putchar('l');
+	_putchar('e');
+	_putchar('\n');
+
+	return (0);
+}
