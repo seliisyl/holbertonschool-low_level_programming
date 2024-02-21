5adb2c7a05ae1ae1e517c92741513ce4ac438b76
seliisyl <8859@holbertonstudents.com>
2024-02-21 21:57:54 +0100

    1-alphabet.c

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -5,14 +5,14 @@
 /**
  * print_alphabet - Prints the alphabetic
  *
- * Return: void
+ * Return:void
  */
 void print_alphabet(void)
 {
 	char c;
 
-	for (c = 'a'; c <= 'z'; c++)
-		 write(1, &c, 1);
+	for (letter = 'a'; letter <= 'z'; letter++)
+		write(1, &c, 1);
 
 	write(1, "\n", 1);
 }
