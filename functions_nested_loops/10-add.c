d1cd68a923a0e4679f52cd2ade6105b07093d985
seliisyl <8859@holbertonstudents.com>
2024-02-23 00:10:10 +0100

    10-add.c

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * add - the function add two integers
+ * @num1: Le premier entier.
+ * @num2: Le deuxième entier.
+ *
+ * Return: The add of the two integers.
+*/
+int add(int num1, int num2)
+{
+	return (num1 + num2);
+}
