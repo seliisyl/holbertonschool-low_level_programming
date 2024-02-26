ff1ac92700df544513fc8260ad423f18451bab22
seliisyl <8859@holbertonstudents.com>
2024-02-26 21:25:46 +0100

    3-print_numbers.c

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -3,13 +3,12 @@
 
 /**
  * print_numbers - Prints numbers from 0 to 9.
- * Return: void
  */
 void print_numbers(void)
 {
 	int i;
 
-	for (i = 0; i < 10; i++)
+	for (i = 0 ; i < 10 ; i++)
 		putchar(i + '0');
 	putchar('\n');
 }
