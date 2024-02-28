0ce0091ed39beb65fb01556ad3c501c59200f4cd
root <root@HBTN-IDF-P1.localdomain>
2024-02-28 15:23:27 +0100

    3-puts.c

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *_puts - Affiche une chaîne de caractères suivie d'un saut de ligne.
+ * @str: Pointeur vers la chaîne de caractères à afficher.
+ *
+ */
+void _puts(char *str)
+{
+	for (; *str != '\0'; str++)
+	{
+		putchar(*str);
+	}
+	_putchar('\n');
+}
