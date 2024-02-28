ea1395ac0e7d1abf94cc3735cc36b3924da9e43e
root <root@HBTN-IDF-P1.localdomain>
2024-02-28 14:48:02 +0100

    2-strlen.c

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strlen - Calcule la longueur d'une chaîne de caractères.
+ * @s: La chaîne de caractères dont la longueur est à calculer.
+ *
+ * Return: Le nombre de caractères dans la chaîne.
+ */
+int _strlen(char *s)
+{
+	size_t len;
+
+	for (len = 0; s[len] != '\0'; len++)
+	{
+	}
+	return (len);
+}
