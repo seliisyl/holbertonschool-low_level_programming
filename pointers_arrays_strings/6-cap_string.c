ff53243995efd39784b45779ef52a15a38449a9f
root <root@HBTN-IDF-P1.localdomain>
2024-03-01 17:04:15 +0100

    6-cap_string.c

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -11,31 +11,23 @@ char *cap_string(char *str)
 {
 	int i = 0;
 
-	while (str[i++])
+	while (str[i])
 	{
-
-		if (str[i - 1] == ' ' ||
-				str[i - 1] == '\t' ||
-				str[i - 1] == '\n' ||
-				str[i - 1] == ',' ||
-				str[i - 1] == ';' ||
-				str[i - 1] == '.' ||
-				str[i - 1] == '!' ||
-				str[i - 1] == '?' ||
-				str[i - 1] == '"' ||
-				str[i - 1] == '(' ||
-				str[i - 1] == ')' ||
-				str[i - 1] == '{' ||
-				str[i - 1] == '}')
+		if (str[i] >= 'a' && str[i] <= 'z')
 		{
-			if (str[i] >= 'a' && str[i] <= 'z')
+			if (i == 0 || str[i - 1] == ' ' ||
+					str[i - 1] == '\t' || str[i - 1] == '\n' ||
+					str[i - 1] == ',' || str[i - 1] == ';' ||
+					str[i - 1] == '.' || str[i - 1] == '!' ||
+					str[i - 1] == '?' || str[i - 1] == '"' ||
+					str[i - 1] == '(' || str[i - 1] == ')' ||
+					str[i - 1] == '{' || str[i - 1] == '}')
 			{
-				str[i] -= 32;
+				str[i] -= 32; /* Conversion en majuscule */
 			}
 		}
 		i++;
 	}
 
 	return (str);
-
 }
