2c13cc925629e7ef4e6d0e9f938db8a3bd5fd2fc
root <root@HBTN-IDF-P1.localdomain>
2024-03-01 12:09:01 +0100

    1-strncat.c

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * char *_strncat - concaténer 2 chaînes mais ajt le nbr d'octets d'entrée
+ * @dest: chaîne à ajouter
+ * @src: chaîne a completera la fin de dest
+ * @n:parametre entiernpour comparer l'index
+ *
+ * Return: retourne une nouvelle caîne concaténer
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int dest_len = 0;
+	int i;
+
+/* calculenla longueur de la chaîne de destinat° */
+	while (dest[dest_len] != '\0')
+	{
+		dest_len++;
+	}
+/* parcourir la chaîne source et concaténer n caractères */
+	while (src[i] != '\0' && i < n)
+	{
+		dest[dest_len + i] = src[i];
+		i++;
+	}
+	/* Ajouter le caractère nul à la fin de la chaîne de destination */
+	dest[dest_len + i] = '\0';
+	return (dest);
+}
