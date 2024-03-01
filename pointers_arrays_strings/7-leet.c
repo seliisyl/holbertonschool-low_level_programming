a51f70351260f58c4b1a032d0cdaa55f3b6ac0ad
root <root@HBTN-IDF-P1.localdomain>
2024-03-01 17:24:49 +0100

    7-leet.c

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -12,14 +12,17 @@ char *leet(char *str)
 	int i1 = 0, i2;
 	char leet[8] = {'0', 'L', '?', 'E', 'A', '?', '?', 'T'};
 
-	while (str[++i1])
+	while (str[i1++])
 	{
 		for (i2 = 0; i2 <= 7; i2++)
 		{
-			if (str[i1] == leet[i2] ||
-					str[i1] - 32 == leet[i2])
+			if (str[i1] == leet[i2] || str[i1] - 32 == leet[i2])
+			{
 				str[i1] = i2 + '0';
+				break;
+			}
 		}
 	}
+
 	return (str);
 }
