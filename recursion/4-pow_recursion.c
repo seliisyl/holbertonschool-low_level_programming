58ec31990a781adf020518f2aa34ff380461b20a
seliisyl <8859@holbertonstudents.com>
2024-03-06 21:37:34 +0000

    4-pow_recursion.c

--- recursion/4-pow_recursion.c
+++ recursion/4-pow_recursion.c
@@ -2,29 +2,25 @@
 #include <stdio.h>
 
 /**
- * _pow_recursion -  Calcule x élev et la puissance y de manière récursive.
+ * _pow_recursion - return value de x augmentéla puisance de y
  * @x: Entier representant de base.
  * @y: Entier représentant l'exposat
  *
- * Retour: Le résultat de x élevé à la puissance y. Si y est ineur 0,
- *         retourne -1 pour indiquer une erreur.
+ * Retour: power number
  */
 int _pow_recursion(int x, int y)
 {
-	/* Vérifie si l'exposant< 0 */
-	if (y < 0)
-	{
-		/* Gère le cas où = 0 */
-		if (x == 0)
-			return (-1); /* Erreur: division par 0 */
-		else
-			return (-1); /* Erreur: résultat fractionnai */
-					}
 
-		/* Cas de base : y = 0 */
-		if (y == 0)
-			return (1);
+	if (y == 0)
+		return (1);
 
+	if (y == 1)
+		return (x); /* Erreur: résultat fractionnai */
+
+	if (y > 1)
 		/* Récursion : multiplie x par lui-même, enecrementant y */
 		return (x * _pow_recursion(x, y - 1));
+
+	else
+		return (-1);
 }
