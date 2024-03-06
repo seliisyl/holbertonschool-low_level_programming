fdd8d2996cdfefba8af31aa06b97b2001cc5b5e5
seliisyl <8859@holbertonstudents.com>
2024-03-06 15:30:03 +0000

    2-strlen_recursion.c

---
+++ recursion/2-strlen_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strlen_recursion - renvoie une longueur d'une cha�ne
+ * @s: Pointeur vers la chaîne de caractères.
+ *
+ * Return: La longueur de la chaîne de caractères.
+ */
+int _strlen_recursion(char *s)
+{
+	if (s[o] != '\0')
+
+		return (1 + _strlen_recursion(s + 1);
+				return(0);
+}
