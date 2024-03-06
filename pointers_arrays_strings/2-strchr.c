857f9d0df08a9383d0646e7ec38472a59e16cf2a
seliisyl <8859@holbertonstudents.com>
2024-03-06 09:20:18 +0000

    2-strchr.c

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -14,14 +14,14 @@ char *_strchr(char *s, char c)
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		if (*s == c)
+		if (s[i] == c)
 		{
-			return (s);
+			return (&s[i]);
 		}
 	}
 	if (c == '\0')
 	{
-		return (s);
+		return (&s[i]);
 	}
 	return (NULL);
 }
