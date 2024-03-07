ec5025ab641be4c636010c81c0283ade5eec0df4
seliisyl <8859@holbertonstudents.com>
2024-03-07 14:35:30 +0000

    0-whatsmyname.c

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - fonction principale
+ * @argc: le nmbr d'argument de la de comde.
+ * @argv: un tableau de string content ls argumt de ligne de comde
+ *
+ * Return: Toujour 0.
+ */
+int main(int __attribute__((unused)) argc, char *argv[])
+{
+	char *nom_programme = argv[0];
+
+	/* argc : Le nombre d'arguments de la ligne de commande.*/
+	/* argv : Un tableau de chaînes contet ls argut de la ligne de comd. */
+	/* Imprimer le nom du programme */
+
+	/* Imprimer le nom du programme */
+	printf("Le nom du programme est : %s\n", nom_programme);
+	return (0);
+}
