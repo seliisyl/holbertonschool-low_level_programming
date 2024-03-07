48edff8731a1cd2c5ec775999e4201d812b9bf51
seliisyl <8859@holbertonstudents.com>
2024-03-07 21:21:46 +0000

    3-mul.c

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -10,30 +10,24 @@
  * Return: Toujours 0.
  */
 int main(int argc, char *argv[])
-{
-	int num1, num2;
-	int result;
-
-	/* Vérifie si le progra reçoit exactement deux arguments*/
+{/* Vérifie si le progra reçoit exactement deux argumen */
 	if (argc != 3)
 	{
-		printf("Erreur\n");
+		printf("%s\n", "Error");
 		/* Affiche "Erreur" suivi d'un saut de ligne */
 		return (1); /* Retourne 1 pour indiquer une erreur */
 	}
 
 	/* Convertit les arguments en entiers */
-	num1 = atoi(argv[1]);
 	/* Convertit le premier argument en entier */
-	num2 = atoi(argv[2]);
-	/* Convertit le deuxième argument en entier*/
+
+	/* Convertit le deuxième argument en entie */
 
 	/* Multiplie les nombres */
-	result = num1 * num2;
 
-	/* Affiche le résulta */
-	printf("%d\n", result);
+	/* Affiche le résulta*/
 
+	printf("%i\n", atoi(argv[1]) * atoi(argv[2]));
 	return (0);
-	/* Retourne 0 pr indiquer que le program c exécuté corm */
+	/* Retourne 0 pr indiquer que le program c exécuté cecte */
 }
