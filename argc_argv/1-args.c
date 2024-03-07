225d1b8be646b6d6f37e45dbed76c47b577a31d6
seliisyl <8859@holbertonstudents.com>
2024-03-07 16:24:23 +0000

    1-args.c

---
+++ argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - fonction principale
+ * @argc: le nmbr d'argument de la de comde.
+ * @argv: un tableau de string content ls argumt de ligne de comde
+ *
+ * Return: Toujour 0.
+ */
+int main(int argc, char *argv[])
+{
+	/* Afficher le nombre d'arguments */
+	printf("%d\n", argc - 1);
+
+	return (0);
+}
+
