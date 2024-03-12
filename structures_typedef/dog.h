a075a618ce877fb1907fd6f0e2c016e3e4c637f3
seliisyl <8859@holbertonstudents.com>
2024-03-12 16:26:50 +0000

    dog.h

---
+++ structures_typedef/dog.h
@@ -0,0 +1,26 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+ * struct dog - dog define
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+ *
+ * Description: Dog definition
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+typedef struct dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+void print_dog(struct dog *d);
+
+#endif /* _DOG_H_ */
