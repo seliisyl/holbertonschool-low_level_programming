0ff11a9e91e5258fce7cfa9680a12c9ac0ad33aa
seliisyl <8859@holbertonstudents.com>
2024-03-12 10:48:29 +0000

    1-string_nconcat.cv

---
+++ more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,48 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * *string_nconcat - concat 2 strings
+ * @s1: 1er string a concat
+ * @s2: 2eme string a concat
+ * @n: n premiersbytes of s2
+ * Return: string a concat
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *ptr = NULL;
+	unsigned int i = 0, j = 0, k;
+
+/* verifier si strings sont NULL es ls init a une string vide si oui */
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
+/* compter la long de s1 et S2 */
+	while (s1[i] != '\0')
+		i++;
+	while (s2[j] != '\0')
+		j++;
+
+/* si n est pls ptt que la long s2, ne concat que ls n 1er caracteres S2 */
+	if (n < j)
+		j = n;
+
+/* Allouer de la mÃmoire pr la nvlle chaÃne */
+	ptr = malloc((i + j + 1) * sizeof(char));
+	if (ptr == NULL)
+		return (NULL);
+/* cp les caractÃres de s1 ds la nvlle chaÃne */
+	for (k = 0; k < i; k++)
+		ptr[k] = s1[k];
+
+/* cp les caractÃresbde s2 (j caract ou moins) ds la nvlle chaÃne */
+	for (k = 0; k < j; k++)
+		ptr[i + k] = s2[k];
+
+/* ajouter le caractÃre de fin de chaÃne */
+	ptr[i + k] = '\0';
+	return (ptr);
+}
