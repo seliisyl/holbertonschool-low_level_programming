27cc4a16789813d84e20eda2d5ede311e2bd5c00
seliisyl <8859@holbertonstudents.com>
2024-03-12 11:18:44 +0000

    2-calloc.c

---
+++ more_malloc_free/2-calloc.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _calloc - alloue m√moire pr un array, en utilisabt malloc
+ * @nmemb: nbr d'√l√ments.
+ * @size: taille de l'√l√ment;
+ * Return: renvoie un ptr vers un arrays d'√l√ment
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	unsigned int i;
+	char *array;
+
+	/* V√©rifier si nmemb ou size est √©ge a 0 */
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	/* allouer de la m√©moire pour le tabeau */
+	array = malloc(nmemb * size);
+	if (array == NULL)
+		return (NULL);
+
+	/* init chq √l√met du tableau a 0 */
+	for (i = 0; i < nmemb * size; i++)
+		*(array + (i * size)) = 0;
+	return (array);
+}
