2cf5df7c8d9892533b8dbf24b479d9b9f75b3394
seliisyl <8859@holbertonstudents.com>
2024-03-13 11:44:56 +0000

    2-print_dog.c

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -2,21 +2,21 @@
 #include <stdio.h>
 
 /**
-  * print_dog - print les détails d'un chine
-  * @d: Pointeur vers la structure dog a imprimer
-  *
-  * Description:
-  * ct fonct� print les détails d'undog, tels que le nom, l'âge et le propro.
-  * Si un él�t de la structure dog est NULL, la fon°print "(nil)"place
-  * Si le pointeur d est NULL, la fonction ne fait rien.
-  */
+ * print_dog - print les détails d'un chine
+ * @d: Pointeur vers la structure dog a imprimer
+ *
+ * Description:
+ * ct fonct� print les détails d'undog, tels que le nom, l'âge et le propro.
+ * Si un éemt de la structure dog est NULL, la fon°print "(nil)"place
+ * Si le pointeur d est NULL, la fonction ne fait rien.
+ */
 void print_dog(struct dog *d)
 {
 	if (d == NULL) /* verifier si le pointeur d est NULL. */
 		return; /* si oui, return sans rien faire */
 
 	if (d->name == NULL) /* verifiez si le nom du chien est NULL */
-		printf("(nil)"); /* si oui return (nil) */
+		printf("name: (nil)\n"); /* si oui return (nil) */
 	else
 		printf("Name: %s\n", d->name);
 
