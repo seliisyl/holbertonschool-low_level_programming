04bd6513fd659a416a7555d4444c0eb0994d14b8
seliisyl <8859@holbertonstudents.com>
2024-04-06 22:53:11 +0000

    0-read_textfile.c

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -19,34 +19,35 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (filename == NULL)
 		return (0);
 
-	buffer = malloc(sizeof(char) * letters);
-	if (buffer == NULL)
-		return (0);
-
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
+		return (0);
+
+	buffer = malloc(sizeof(char) * (letters + 1));
+	if (buffer == NULL)
 	{
-		free(buffer);
+		close(fd);
 		return (0);
 	}
 
 	bytes_read = read(fd, buffer, letters);
 	if (bytes_read == -1)
 	{
-		free(buffer);
 		close(fd);
+		free(buffer);
 		return (0);
 	}
+
+	buffer[bytes_read] = '\0';
 
 	bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
 	if (bytes_written == -1 || bytes_written != bytes_read)
 	{
-		free(buffer);
 		close(fd);
+		free(buffer);
 		return (0);
 	}
-
-	free(buffer);
 	close(fd);
-	return (bytes_written);
+	free(buffer);
+	return (bytes_read);
 }
